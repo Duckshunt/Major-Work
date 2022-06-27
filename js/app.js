20dd37dd5ecd1584a69072cd0833e0a4e5b62c5b
Duckshunt <80522391+Duckshunt@users.noreply.github.com>
2022-06-27 14:44:25 +1000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+var element = document.body;
+
+var theme = localStorage.getItem('theme')
+x = document.getElementById("change-icon")
+function page_load(){
+    console.log(theme)
+    if (theme != "light-mode"){
+        theme_load()
+    }
+  }
+function theme_load(){
+    x.classList.toggle("fa-sun");
+    element.classList.toggle("dark-mode");
+}
+ 
+function theme_change() {
+
+    if (theme == "light-mode") {
+        x.classList.toggle("fa-sun");
+        element.classList.toggle("dark-mode");
+    
+        theme = "dark-mode"
+        console.log(theme)
+    } else { 
+        x.classList.toggle("fa-sun");
+        element.classList.toggle("dark-mode");
+        theme = "light-mode"
+        console.log(theme)
+    }
+    
+    
+  }
+window.addEventListener('unload', function(event) {
+    localStorage.setItem('theme', theme);
+    console.log(localStorage.getItem('theme'));
+})
