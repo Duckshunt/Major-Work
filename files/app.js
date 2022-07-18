3ab08b102d02b97fb1b9216f6d42e04eea49e97f
Duckshunt <80522391+Duckshunt@users.noreply.github.com>
2022-07-18 21:05:30 +1000

    working

--- files/app.js
+++ files/app.js
@@ -15,18 +15,19 @@ function theme_load(){
 }
  
 function theme_change() {
+    
 
     if (theme == "light-mode") {
         x.classList.toggle("fa-sun");
         element.classList.toggle("dark-mode");
     
         theme = "dark-mode"
-        console.log(theme)
+        
     } else { 
         x.classList.toggle("fa-sun");
         element.classList.toggle("dark-mode");
         theme = "light-mode"
-        console.log(theme)
+        
     }
     
   }
@@ -34,4 +35,52 @@ window.addEventListener('unload', function(event) {
     localStorage.setItem('theme', theme);
     console.log(localStorage.getItem('theme'));
 })
+
+var model_list = {
+    
+}
+console.log(model_list)
+
+
+function changecat(value) {
+    if (value.length == 0) document.getElementById("models").innerHTML = "<option></option>";
+    else {
+        var catOptions = "<option disabled>Select a model</option>";
+        for (categoryId in model_list[value]) {
+            catOptions += "<option>" + model_list[value][categoryId] + "</option>";
+        }
+        document.getElementById("models").innerHTML = catOptions;
+    }
+}
+
+
+
+fetch('http://localhost:3000/data')
+    .then(res => res.json())
+    .then((out) => {
+        //console.log('Output: ', out);
+        //console.log(out.brands_list)
+        //console.log(typeof(out))
+        var x = document.getElementById("brands")
+        var l = out.brands_list
+        var m = out.model_list
+        //console.log(out.model_list)
+        //console.log(m)
+        model_list = Object.assign(model_list, m)
+        //console.log(model_list)
+        //const obj = JSON.parse(l)
+        //console.log(l)
+        //console.log(obj, 'Object')
+        //console.log(Object.values(l))
+        //console.log(l[2])
+        for (let i = 0; i < l.length; i++) {
+            var c = document.createElement("option")
+            //console.log(l[i])
+            c.text = l[i];
+            v = l[i].replace(' ', '-')
+            c.classList.add(v);
+            x.options.add(c,i+1);
+          }
+        changecat("Audi")
+}).catch(err => console.error(err));
 
--- server.js
+++ server.js
@@ -5,15 +5,26 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-mongoose.connect("mongodb+srv://Ducksrub:CrvSpTLmXCZfC2Wc@data.dvfjsbk.mongodb.net/notes")
+//mongoose.connect("mongodb+srv://Ducksrub:CrvSpTLmXCZfC2Wc@data.dvfjsbk.mongodb.net/notes")
+mongoose.connect("mongodb+srv://Ducksrub:CrvSpTLmXCZfC2Wc@data.dvfjsbk.mongodb.net/cars")
 
 const notesSchema = {
     title: String,
     content: String
 }
+const carSchema = {
+    make: String,
+    model: String,
+    battery_size: Number,
+    range: Number,
+    price: Number,
+    type: String
+}
 
-const Note = mongoose.model("Note", notesSchema)
-
+//const Note = mongoose.model("Note", notesSchema)
+const Car = mongoose.model("electric_car", carSchema)
+var brands_list = []
+var model_list = {}
 
 
 app.use(express.static("files"))
@@ -22,10 +33,85 @@ app.use(bodyParser.json())
 
 // define the first route
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/files/home.html")
+    
+    res.sendFile(__dirname + "/html/home.html")
+    console.log('hi')
     //res.sendFile("home.html")
 })
 
+app.get("/home.html", function (req, res) {
+    
+    res.sendFile(__dirname + "/html/home.html")
+    console.log('hi2')
+    //res.sendFile("home.html")
+})
+
+app.get("/find_car.html", function (req, res) {
+    console.log(req.query.brands)
+    var query = Car.distinct('make');
+    
+    //var brands = document.getElementById("brands").innerHTML
+    //var c = document.createElement("option")
+    //query.distinct('make')
+
+    query.exec(function(err,car){
+        if (err) return err
+        //console.log(car);
+        for (let i = 0; i < car.length; i++) {
+            //console.log(car[i].split())
+            if (!brands_list.includes(car[i])) {
+                brands_list.push(car[i])
+              }
+          }
+        //console.log(brands_list, "hi")
+
+    })
+    var model_query = Car.find({}).select({make:1, model:1, _id:0});
+    model_query.exec(function(err,models){
+        if (err) return err
+        //console.log(models);
+        //console.log(typeof(models[0]))
+        //console.log(models.make)
+        for (let i = 0; i < models.length; i++) {
+            //console.log(models[i])
+            x = models[i].make
+            y = models[i].model
+            //console.log(x,y)
+            if (!model_list[x]) {
+                model_list[x] = [];
+                
+            }
+            //console.log(model_list[x])
+            if (!model_list[x].includes(y)) {
+                model_list[x].push(y)
+            }
+            
+        }
+        //console.log(model_list)
+    })
+
+    
+    //console.log(brand_list +"hi");
+    res.sendFile(__dirname + "/html/find_car.html")
+    //res.sendFile("home.html")
+})
+
+app.get("/data", function(req,res){
+    //console.log(model_list)
+    res.json({brands_list, model_list})
+ 
+    //res.send(brands_list)
+    //console.log('brands')
+})
+
+app.get("/login.html", function (req, res) {
+    
+    res.sendFile(__dirname + "/html/login.html")
+    //console.log('hi')
+    //res.sendFile("home.html")
+})
+
+/*
 app.post("/files/find_car.html", function(req,res){
     let newNote = new Note({
         title: req.body.title,
@@ -35,7 +121,23 @@ app.post("/files/find_car.html", function(req,res){
     res.redirect("/find_car.html");
     
 })
+*/
 
+/*
+var query = Car.find({});
+
+query.select('make')
+
+query.exec(function(err,car){
+    if (err) return err
+    console.log(car)
+})
+*/
+
+
+
+//console.log(Car.find({}));
+//console.log(Car.find({ model: 'Audi'}, function (err, docs) {}));
 
 
 // start the server listening for requests
